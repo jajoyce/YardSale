d5bbb188e95644ca15c77ca2145c1b03825b3728
mikeyj913 <mikeyj913@hotmail.com>
2022-01-25 14:19:08 -0500

    Updated seedData users added 4 people

--- controllers/seed_data/user.js
+++ controllers/seed_data/user.js
@@ -7,4 +7,33 @@ module.exports = [
         profileImage: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.dbmcpa.com%2Fwp-content%2Fuploads%2F2017%2F07%2Fgeneric-female-headshot.png&f=1&nofb=1", 
         averageRating: 3.5, 
     }, 
+    {
+        userName: "AliceWalker123", 
+        firstName: "Jane", 
+        lastName: "Doe", 
+        location: "New York", 
+        profileImage: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.0W5W4xqVXEn_q25EhSdm5QHaJQ%26pid%3DApi&f=1", 
+        averageRating: 2.1, 
+    }, {
+        userName: "chrisDollars$$", 
+        firstName: "Christopher", 
+        lastName: "Augustine", 
+        location: "Boston", 
+        profileImage: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.uni-regensburg.de%2FFakultaeten%2Fphil_Fak_II%2FPsychologie%2FPsy_II%2Fbeautycheck%2Fenglish%2Fprototypen%2Fm_sexy_gr.jpg&f=1&nofb=1", 
+        averageRating: 4.4, 
+    }, {
+        userName: "MaryWisdom21", 
+        firstName: "Marissa", 
+        lastName: "Wisdom", 
+        location: "New York", 
+        profileImage: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.CUnT6hA2NQLYYVHKLsyEiwAAAA%26pid%3DApi&f=1", 
+        averageRating: 3.0, 
+    }, {
+        userName: "defaultUser15", 
+        firstName: "Jenny", 
+        lastName: "Tua", 
+        location: "Boston", 
+        profileImage: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.QVd2TToiairiy_E28sOYhQAAAA%26pid%3DApi&f=1", 
+        averageRating: 4.9, 
+    }, 
 ];
