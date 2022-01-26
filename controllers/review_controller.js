d93a0460d9961d2166ef8211efd5544b08f3d0d1
jajoyce <jjoyce4@gmail.com>
2022-01-26 12:12:14 -0500

    Set up review controller to insert seed data in db and pass data to reviews view index.ejs.

--- controllers/review_controller.js
+++ controllers/review_controller.js
@@ -3,11 +3,23 @@ const router = express.Router();
 const { Review } = require('../models');
 const seedData = require('./seed_data/review');
 
+Review.deleteMany({}, (error, deletedReviews) => {
+    if (error) return console.log(error);
+    console.log(deletedReviews);
+    Review.insertMany(seedData, (error, insertedReviews) => {
+        if (error) return console.log(error);
+        console.log('REVIEWS SEED COMPLETE:');
+        console.log(insertedReviews);
+        }
+    );
+});
+
 
 router.get('/', (req, res) => {
-    console.log('SEED DATA:');
-    console.log(seedData);
-    res.render('reviews/index.ejs');
+    Review.find({}, (error, foundReviews) => {
+        if (error) return console.log(error);
+        res.render('reviews/index.ejs', { reviews: foundReviews });
+    });
 });
 
 module.exports = router;
