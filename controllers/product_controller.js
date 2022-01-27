dbbf7e62d700ca6a0d461faa9c40d5984c35dc8b
jajoyce <jjoyce4@gmail.com>
2022-01-27 06:19:25 -0500

    Refactor new product route with async funtion and pass in default user for new product form.

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -13,8 +13,14 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/new', (req, res) => {
-    res.render('products/new.ejs');
+router.get('/new', async (req, res) => {
+    try {
+        const defaultUser = await User.find({})[0];
+        return res.render('products/new.ejs', {user: defaultUser});
+    } catch (error) {
+        console.log(error);
+        return res.redirect('/products');
+    }
 });
 
 router.get('/:productID', async (req, res) => {
