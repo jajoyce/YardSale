33e26436f51810f3bc08b3d9d6cbca2b7a8da309
jajoyce <jjoyce4@gmail.com>
2022-01-28 13:20:16 -0500

    Revert config

--- config/db.connection.js
+++ config/db.connection.js
@@ -5,7 +5,7 @@ const connectionString = process.env.MONGODB_URI;
 
 mongoose.connect(
     connectionString,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
       console.log('Connected to MongoDB');
     }
